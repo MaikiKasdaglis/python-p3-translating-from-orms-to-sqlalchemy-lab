ad0933690d29d0e8b874715c733c40c6ed4d1c67
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-27 15:15:04 -0600

    finishes lab

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,29 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    base.metadata.create_all(bind=engine)
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    return session.query(Dog).all()
+
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter_by(name=name).first()
+    return dog
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter_by(id=id).first()
+    return dog 
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter(Dog.name == name, Dog.breed == breed).first()
+    return dog 
 
 def update_breed(session, dog, breed):
-    pass
+    dog = session.query(Dog).filter_by(id=dog.id).first()
+    dog.breed = breed
+    session.commit()
